75570629136cf1f6090768c0900affcea235b1a1
Chaos Coder <dinakar.anumolu@gmail.com>
2025-07-09 11:35:45 -0500

    added AD Transaction Analysis Fields and Splunk log data files

---
+++ djangoproject/djangoapp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, redirect
+from .forms import UserRegistrationForm
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save(commit=False)
+            new_user.set_password(form.cleaned_data['password'])
+            new_user.save()
+            return render(request, 'registration/register_done.html', {'user': new_user})
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'registration/register.html', {'form': form})
